a391997f59368ff0f1258e903bbebdf893dc53ba
Alyona Mustafina <alyona.mustafina@yandex.ru>
2019-04-19 21:09:13 +0300

    continue with html css

--- Task2/Task2.Web/Startup.cs
+++ Task2/Task2.Web/Startup.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using Castle.Core.Interceptor;
 using Castle.DynamicProxy;
@@ -114,6 +115,7 @@ namespace Task2.Web
 
 				if (env.IsDevelopment())
 				{
+					spa.Options.StartupTimeout = new TimeSpan(days: 0, hours: 0, minutes: 1, seconds: 30);
 					spa.UseAngularCliServer(npmScript: "start");
 				}
 			});
