00cc27f40530597146eae911bc6854e685d15eb5
KirillKolyshkin <40914279+KirillKolyshkin@users.noreply.github.com>
2019-05-11 13:24:44 +0300

    new commit

--- Task2/Task2.Infrastructure/Migrations/20190511102158_new.Designer.cs
+++ Task2/Task2.Infrastructure/Migrations/20190511102158_new.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Task2.Infrastructure;
@@ -9,9 +10,10 @@ using Task2.Infrastructure;
 namespace Task2.Infrastructure.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20190511102158_new")]
+    partial class @new
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Task2/Task2.Infrastructure/Migrations/20190511102158_new.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Task2.Infrastructure.Migrations
+{
+    public partial class @new : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Genre",
+                table: "Posts",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Performer",
+                table: "Posts",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Genre",
+                table: "Posts");
+
+            migrationBuilder.DropColumn(
+                name: "Performer",
+                table: "Posts");
+        }
+    }
+}
